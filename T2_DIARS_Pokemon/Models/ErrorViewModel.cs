e57d119507b2f34570c10aa79b267d6a02e0fa51
Elard Burga Goicochea <elard3d@hotmail.com>
2020-10-14 19:54:28 -0500

    Agregar archivos de proyecto.

---
+++ T2_DIARS_Pokemon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace T2_DIARS_Pokemon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
